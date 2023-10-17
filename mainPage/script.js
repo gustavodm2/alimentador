8a48886b4ac9e6a5927464776b3026f9928db9a1
gustavodm2 <114183269+gustavodm2@users.noreply.github.com>
2023-10-17 17:07:33 -0300

    ba

---
+++ mainPage/script.js
@@ -0,0 +1,37 @@
+const scrollableContent = document.querySelector(".scrollable-content");
+
+document.querySelector(".fa-plus").addEventListener("click", () => {
+    const newTextbox = document.createElement("div");
+    newTextbox.className = "textbox";
+
+    const clockIcon = document.createElement("i");
+    clockIcon.className = "fas fa-clock";
+    clockIcon.style.padding = "12px"; 
+
+    const trashIcon = document.createElement("i");
+    trashIcon.className = "fas fa-trash";
+    trashIcon.style.padding = "12px"; 
+
+    const timeInput = document.createElement("input");
+    timeInput.type = "time";
+    timeInput.style.display = "none"; 
+
+    newTextbox.appendChild(clockIcon);
+    newTextbox.appendChild(timeInput);
+    newTextbox.appendChild(trashIcon);
+
+    scrollableContent.appendChild(newTextbox);
+
+    trashIcon.addEventListener("click", () => {
+        scrollableContent.removeChild(newTextbox);
+    });
+
+    clockIcon.addEventListener("click", () => {
+        timeInput.style.display = "block";
+    });
+
+    timeInput.addEventListener("change", () => {
+        newTextbox.replaceChild(document.createTextNode(timeInput.value), timeInput);
+    });
+});
+
