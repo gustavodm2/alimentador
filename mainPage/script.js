d174ffea792a28b6ee6918f2d498fa99a1f39b3f
picoliW <114180339+picoliW@users.noreply.github.com>
2023-10-18 09:59:21 -0300

    Add files via upload

--- mainPage/script.js
+++ mainPage/script.js
@@ -35,3 +35,19 @@ document.querySelector(".fa-plus").addEventListener("click", () => {
     });
 });
 
+
+function openLoginPopup() {
+    document.getElementById("overlay").style.display = "block";
+    document.getElementById("loginPopup").style.display = "block";
+}
+
+function closeLoginPopup() {
+    document.getElementById("overlay").style.display = "none";
+    document.getElementById("loginPopup").style.display = "none";
+}
+
+document.getElementById("loginPopup").addEventListener("click", function (event) {
+    event.stopPropagation();
+    })
+
+
