0081b48e6cedfb6f17e7121998ab7b882bcbf000
Otávio Costa <106917785+otavioCosta2110@users.noreply.github.com>
2023-11-24 16:28:00 -0300

    secrets do tf adicionado

---
+++ terraform/secrets.tf
@@ -0,0 +1,23 @@
+variable "rds_user" {
+  type    = string
+  default = "usuario_do_rds"
+}
+variable "rds_password" {
+  type    = string
+  default = "senha_do_rds"
+}
+variable "rds_db_name" {
+  type    = string
+  default = "nome_da_database"
+}
+
+
+variable "mqtt_user" {
+  type    = string
+  default = "usuario_do_mqtt"
+}
+variable "mqtt_password" {
+  type    = string
+  default = "senha_do_mqtt"
+}
+
